b95e0f74950bde88bd823152bdedd32bec4a1964
Varun Maharana <varunmaharana14@gmail.com>
2026-01-29 01:37:55 +0530

    Api success flow updated

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,21 +3,4 @@ import { BadRequestError } from "../lib/utils/CustomError";
 import { asyncHandler } from "../lib/utils/asyncHandler";
 import { ApiResponse } from "../lib/utils/ApiResponse";
 
-export const register = asyncHandler(async (req: Request, res: Response) => {
-	// const { name, email, password, phone } = req.body;
-
-	// res.status(201).json(
-	// 	new ApiResponse(
-	// 		200,
-	// 		{
-	// 			user: {
-	// 				name: "John Doe",
-	// 				email: "john.doe@example.com",
-	// 				phone: "1234567890",
-	// 			},
-	// 		},
-	// 		"User registered successfully",
-	// 	),
-	// );
-	throw new BadRequestError("Invalid Data Provided");
-});
+export const register = asyncHandler(async (req: Request, res: Response) => {});
--- src/lib/utils/ApiResponse.ts
+++ src/lib/utils/ApiResponse.ts
@@ -1,15 +1,30 @@
+import { Response } from "express";
 class ApiResponse {
-    statusCode: number;
-    payload: any;
-    message: string;
-    success: boolean;
+	statusCode: number;
+	payload: any;
+	message: string;
+	success: boolean;
 
-    constructor(statusCode: number, payload: any, message: string = "Success") {
-        this.statusCode = statusCode;
-        this.payload = payload;
-        this.message = message;
-        this.success = statusCode < 400;
-    }
+	constructor(statusCode: number, payload: any, message: string = "Success") {
+		this.statusCode = statusCode;
+		this.payload = payload;
+		this.message = message;
+		this.success = statusCode < 400;
+	}
+
+	static send(
+		res: Response,
+		statusCode: number,
+		payload: any,
+		message: string = "Success",
+		success: boolean = true,
+	) {
+		res.status(statusCode).json({
+			success,
+			message,
+			payload,
+		});
+	}
 }
 
 export { ApiResponse };
